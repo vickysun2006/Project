c5b6a1d83fb8e47e603a9d01f42b490b79595f9b
vickysun2006 <vicky.sun2006@hotmail.com>
2024-12-07 22:14:30 +0800

    Create alumniData.ts

---
+++ data/alumniData.ts
@@ -0,0 +1,9 @@
+import { Alumni } from '../types/alumni';
+
+export const alumniData: Alumni[] = [
+  { id: 1, name: "张三", city: "北京", latitude: 39.9042, longitude: 116.4074 },
+  { id: 2, name: "李四", city: "上海", latitude: 31.2304, longitude: 121.4737 },
+  { id: 3, name: "王五", city: "广州", latitude: 23.1291, longitude: 113.2644 },
+  { id: 4, name: "赵六", city: "深圳", latitude: 22.5431, longitude: 114.0579 },
+  { id: 5, name: "钱七", city: "杭州", latitude: 30.2741, longitude: 120.1551 },
+];
