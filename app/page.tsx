5726bca4eb97ca7eed78aae6fb55c2ddf101493e
vickysun2006 <vicky.sun2006@hotmail.com>
2024-12-07 22:13:26 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import AlumniMap from '../components/AlumniMap'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <AlumniMap />
+    </main>
+  )
+}
